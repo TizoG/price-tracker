72f39bc94a5b8531f58b753ba46a2f093fc6b65f
TizoG <tirado114@hotmail.com>
2025-04-23 17:30:04 +0200

    organizando los modelos en archivos separados

---
+++ backend/app/models/products.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, Float, ForeignKey, TIMESTAMP, func
+from sqlalchemy.orm import relationship
+from backend.app.bbdd.database import Base
+
+
+class Products(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    price = Column(Float, nullable=False)
+    image = Column(Text, nullable=False)
+
+    price_history = relationship(
+        "PriceHistory", back_populates="products", cascade="all, delete")
