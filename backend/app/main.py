9900cb98e1cfa4ceedfc96226302cbb22c407810
TizoG <tirado114@hotmail.com>
2025-04-24 11:31:14 +0200

    Creacion de schemas, router y CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import sessionmaker
 from bbdd.database import Base, local_session, engine
 from models.price_history import PriceHistory
 from models.products import Products
 
-
+# Instaciamos la app de FastAPI
 app = FastAPI()
 
+# Incluimos las rutas
+app.include_router(local_session)
 
+# Configuramos CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Crear Tablas
+
+
 def crear_tablas():
     try:
         Base.metadata.create_all(bind=engine)
@@ -17,5 +30,6 @@ def crear_tablas():
         print(f"Error al crear las tablas: {e}")
 
 
+# instanciamos las funciones
 if __name__ == "__main__":
     crear_tablas()
---
+++ backend/app/schemas/price_history_schema.py
@@ -0,0 +1,11 @@
+import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+
+class PriceHistorySchema(BaseModel):
+    id: int
+    product_id: int
+    date_scraping: datetime
+    price: float
+    source: Optional[str]  # Permite que sea null
